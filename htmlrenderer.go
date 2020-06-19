59161bdab17bfb481f9f9d9785f0ee0b3937461a
Denis Badurina <badurinadenis@gmail.com>
2020-06-19 17:52:02 +0200

    feat(htmlrenderer): introduce table config

--- htmlrenderer.go
+++ htmlrenderer.go
@@ -6,6 +6,7 @@ import (
 	"html"
 	"io"
 	"reflect"
+	"strings"
 
 	"github.com/domonda/go-types/txtfmt"
 	fs "github.com/ungerik/go-fs"
@@ -18,16 +19,29 @@ type HTMLFormatRenderer interface {
 	Caption() string
 }
 
+// HTMLTableConfig is the config for the actual, visual, resulting HTML table.
+type HTMLTableConfig struct {
+	TableClass      string
+	CaptionClass    string
+	RowClass        string
+	CellClass       string
+	HeaderRowClass  string
+	HeaderCellClass string
+	DataRowClass    string
+	DataCellClass   string
+}
+
 // HTMLRenderer implements Renderer by using a HTMLFormatRenderer
 // for a specific text based table format.
 type HTMLRenderer struct {
-	format HTMLFormatRenderer
-	config *txtfmt.FormatConfig
-	buf    bytes.Buffer
+	format      HTMLFormatRenderer
+	config      *txtfmt.FormatConfig
+	tableConfig *HTMLTableConfig
+	buf         bytes.Buffer
 }
 
-func NewHTMLRenderer(format HTMLFormatRenderer, config *txtfmt.FormatConfig) *HTMLRenderer {
-	return &HTMLRenderer{format: format, config: config}
+func NewHTMLRenderer(format HTMLFormatRenderer, config *txtfmt.FormatConfig, tableConfig *HTMLTableConfig) *HTMLRenderer {
+	return &HTMLRenderer{format: format, config: config, tableConfig: tableConfig}
 }
 
 func (htm *HTMLRenderer) RenderHeaderRow(columnTitles []string) error {
@@ -36,23 +50,39 @@ func (htm *HTMLRenderer) RenderHeaderRow(columnTitles []string) error {
 		return err
 	}
 
-	err = htm.write("<table>\n")
+	if htm.tableConfig.TableClass != "" {
+		err = htm.write("<table class='%s'>\n", html.EscapeString(htm.tableConfig.TableClass))
+	} else {
+		err = htm.write("<table>\n")
+	}
 	if err != nil {
 		return err
 	}
 	caption := htm.format.Caption()
 	if caption != "" {
-		err = htm.write("<caption>%s</caption>\n", caption)
+		if htm.tableConfig.CaptionClass != "" {
+			err = htm.write("<caption class='%s'>%s</caption>\n", caption, html.EscapeString(htm.tableConfig.CaptionClass))
+		} else {
+			err = htm.write("<caption>%s</caption>\n", caption)
+		}
 		if err != nil {
 			return err
 		}
 	}
-	err = htm.write("<tr>\n")
+	if htm.tableConfig.HeaderRowClass != "" || htm.tableConfig.RowClass != "" {
+		err = htm.write("<tr class='%s'>\n", strings.TrimSpace(htm.tableConfig.HeaderRowClass+" "+htm.tableConfig.RowClass))
+	} else {
+		err = htm.write("<tr>\n")
+	}
 	if err != nil {
 		return err
 	}
 	for _, columnTitle := range columnTitles {
-		err = htm.write("<th>%s</th>", columnTitle)
+		if htm.tableConfig.HeaderCellClass != "" || htm.tableConfig.CellClass != "" {
+			err = htm.write("<th class='%s'>%s</th>", columnTitle, strings.TrimSpace(htm.tableConfig.HeaderCellClass+" "+htm.tableConfig.CellClass))
+		} else {
+			err = htm.write("<th>%s</th>", columnTitle)
+		}
 		if err != nil {
 			return err
 		}
@@ -62,7 +92,12 @@ func (htm *HTMLRenderer) RenderHeaderRow(columnTitles []string) error {
 }
 
 func (htm *HTMLRenderer) RenderRow(columnValues []reflect.Value) error {
-	err := htm.write("<tr>\n")
+	var err error
+	if htm.tableConfig.DataRowClass != "" || htm.tableConfig.RowClass != "" {
+		err = htm.write("<tr class='%s'>\n", strings.TrimSpace(htm.tableConfig.DataRowClass+" "+htm.tableConfig.RowClass))
+	} else {
+		err = htm.write("<tr>\n")
+	}
 	if err != nil {
 		return err
 	}
@@ -76,7 +111,11 @@ func (htm *HTMLRenderer) RenderRow(columnValues []reflect.Value) error {
 			str = html.EscapeString(str)
 		}
 
-		err := htm.write("<td>%s</td>", str)
+		if htm.tableConfig.DataCellClass != "" || htm.tableConfig.CellClass != "" {
+			err = htm.write("<td class='%s'>%s</td>", str, strings.TrimSpace(htm.tableConfig.DataCellClass+" "+htm.tableConfig.CellClass))
+		} else {
+			err = htm.write("<td>%s</td>", str)
+		}
 		if err != nil {
 			return err
 		}
